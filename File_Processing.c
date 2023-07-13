eaece4cb02c56436000cba8576ad033a6c69fd8a
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-07-13 13:52:48 +0530

    created a function to get length

--- File_Processing.c
+++ File_Processing.c
@@ -2,6 +2,17 @@
 #include<string.h>
 #define SIZE 1000
 
+int textLength(char*);
+
+//Get length of each text line
+int textLength(char* array){
+    int x = 0;
+    while(array[x] != '\0'){
+        x++;
+    }
+    return x;
+}
+
 int main(){
 
     FILE *fp;
@@ -11,12 +22,15 @@ int main(){
     printf("Enter a search string: ");
     scanf("%[^\n]",pattern);
 
-    fp = fopen("./modules.txt","r");
+    fp = fopen("./modules.txt","r"); //Import text file
 
     if(fp == NULL){
         printf("\n Error!!! Can't open the file.\n");
         return 0;
     }
 
+    plen = textLength(pattern);
     
+    
+
 }

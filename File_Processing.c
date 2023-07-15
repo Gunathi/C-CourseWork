959b0eeb3aad411a0553f26b2bf878c6e9f4c300
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-07-15 21:42:33 +0530

    Update File_Processing.c

--- File_Processing.c
+++ File_Processing.c
@@ -3,6 +3,7 @@
 #define SIZE 1000
 
 int textLength(char*);
+char *setCase(char *txt);
 
 //Get length of each text line
 int textLength(char* array){
@@ -11,6 +12,42 @@ int textLength(char* array){
         x++;
     }
     return x;
+}
+
+char *setCase(char txt[]){
+    for(int i = 0; i < strlen(txt) i++){
+        if(txt[i] >= 65 && txt[i] <= 90){
+            txt[i] = txt[i] + 32;
+        }
+    }
+    return txt;
+}
+
+int searchKMP(char *T, char *P){
+
+    int m = strlen(P), n = strlen(T), i = 0, j = 0;
+    int* PSuffix = preProcess(P, strlen(p));
+
+    found = 0; 
+
+    while(i < n){
+        if(P[j] == T[i]){
+            j++;
+            i++;
+        }
+
+        if(j == m){
+            found = 1;
+            j = PSuffix[j - 1];
+        }else if(j < n && P[j] != [i]){
+            if(j != 0){
+                j = PSuffix[j - 1];
+            }else {
+                i++;
+            }
+        }
+    }
+    return found;
 }
 
 int main(){
@@ -31,6 +68,27 @@ int main(){
 
     plen = textLength(pattern);
     
-    
+    printf("\n Search Result : \n");
+
+    while(ch != EOF){
+
+        fscanf(fp, "%[^\n]", text);
+        tlen = textLength(text);
+
+        strcpy(T, text); //Copy the string in text variable to variable T
+        strcpy(P, pattern); //Copy the string in pattern variable to variable P
+
+        setCase(T);
+        setCase(P);
+
+        searchKMP(T, P);
+
+        if(found == 1){
+            comp++;
+            printf(" * %s\n", text);
+        }
+
+        ch = getc(fp);
+    }
 
 }
